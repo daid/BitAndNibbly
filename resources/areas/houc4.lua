1d7c39ec9a2cccee3265dbde3afda40aacb96514
daid <daid303@gmail.com>
2019-06-27 17:08:55 +0200

    Fix the text going off-screen.

--- resources/areas/houc4.lua
+++ resources/areas/houc4.lua
@@ -1,5 +1,5 @@
 sign.onUse(function(player)
-    message(sign, "This is just an early alpha.")
-    message(sign, "Maybe there will be more some day.")
+    message(sign, "This is just an\nearly alpha.")
+    message(sign, "Maybe there will\nbe more some day.")
     message(sign, "Signed:\n-Daid")
 end)
